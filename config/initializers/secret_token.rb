60f995d902edbce21cc75b5aa3289a70af8bc623
alexanderkjones <alexanderkjones@gmail.com>
2013-06-16 15:49:28 -0400

    Fresh Rails Install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apiara::Application.config.secret_token = 'e916b56ab5f12ba045ef3684832d14471d44a5bc221ec5e8b9e1d736d959b2e6025f11efd174bb40f3eed1eedc8f66e0cdb05d896b24eadc15df6e65dfb8e356'
