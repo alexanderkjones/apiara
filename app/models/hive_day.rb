d1bc84190c0a8b2223e60f7a4df3c94ffea5100b
Anthony Mitchell <awmitchell@gmail.com>
2013-06-28 20:21:34 -0400

    methods to add to population_cumulative and production_cumulative added. also added method called daily_tasks that could be called to create the new entry in the HiveDays table. it creates the table connection, calls the methods that need to be called, then creates the record. right now i'm calling it with an id, population, and production. that will be changed once the production and population finding methods are done. those options are there for testing.

--- app/models/hive_day.rb
+++ app/models/hive_day.rb
@@ -1,14 +1,42 @@
 class HiveDay < AWS::Record::HashModel
-  string_attr :id #HashKey
-  datetime_attr :date #RangeKey
+  string_attr :id #HashKey, this will be the hiveid
+  string_attr :date_time #RangeKey
   
-  string_attr :hiveid
   integer_attr :population #counter
   integer_attr :population_cumulative #today's pop + yesterday's population_cumulative
   integer_attr :production #counter
   integer_attr :production_cumulative #today's production + yesterday's production_cumulative
   
-  def add_to_pop_counter
+  def self.daily_tasks(id, population, production)
+    #table connection stuff
+    db = AWS::DynamoDB.new
+    table = db.tables['apiara_development_HiveDay']
+    
+    table.hash_key = [:id, :string]
+    table.range_key = [:date_time, :string]
+    
+    range_low = 1.5.days.ago.to_i.to_s
+    range_high = Time.now.to_i.to_s
+
+    #get the data from yesterday
+    the_array = []
+    collection = table.items.query(:hash_value => id, :range_value => range_low..range_high, 
+                                    :select => [:population_cumulative, :production_cumulative])
+
+    collection.each do |item_data|
+      the_array.push(item_data.attributes)
+    end
+
+    population_cumulative = calc_new_population_cumulative(population, the_array.first["population_cumulative"])
+    production_cumulative = calc_new_production_cumulative(production, the_array.first["production_cumulative"])
+    
+    hiveday = HiveDay.create(:id => id, :date_time => Time.now.to_i.to_s, :population => population, 
+                              :population_cumulative => population_cumulative, :production => production, 
+                              :production_cumulative => production_cumulative)
+    hiveday.save
+  end
+  
+  def self.add_to_pop_counter
     # Build model method that adds to pop_counter as a function of 
     # the change in weight from sunrise to the bottom of the bell 
     # curve just before bees start coming back to the hive. Said 
@@ -18,18 +46,18 @@ class HiveDay < AWS::Record::HashModel
     # coming home.
   end
   
-  def calc_new_population_cumulative
+  def self.calc_new_population_cumulative(population, yesterday_population_cumulative)
     # add sum total population today to yesterday's total population
-    # population_cumulative = yesterday_population_cumulative + population
+    return population + yesterday_population_cumulative
   end
   
-  def calc_todays_production
+  def self.calc_todays_production
     # subtract beginning of the day from end of the day's weight to get today's production
     # production = end_weight - beginning_weight
   end
   
-  def calc_new_production_cumulative
+  def self.calc_new_production_cumulative(production, yesterday_production_cumulative)
     # add sum total population today to yesterday's total population
-    # production_cumulative = yesterday_production_cumulative + production
+    return production + yesterday_production_cumulative
   end
 end
