cb0c555e7c5d742560ff766be751b64883251305
Anthony Mitchell <awmitchell@gmail.com>
2013-08-02 18:36:54 -0400

    added basic hive_event creation method as well as a method that will send email alerts.

--- app/models/hive_event.rb
+++ app/models/hive_event.rb
@@ -5,4 +5,45 @@ class HiveEvent < AWS::Record::HashModel
   string_attr :data_snapshot
   string_attr :type
   string_attr :details  
+
+  #create new hive event
+  def self.new_event(hive_id, data_snapshot, type, details)
+    #create new HiveEvent record
+    hiveday = HiveEvent.create(:id => hive_id, :date_time => Time.now.to_i, :data_snapshot => data_snapshot,
+                                :type => type, :details => details)
+    hiveday.save
+  end
+  
+  #this method will send alert emails to users based on what event is reported
+  def self.send_alert(type)
+    #i would be willing to bet there is a MUCH better way to do this part...
+    ses = AWS::SimpleEmailService.new(:access_key_id => 'AKIAIGCGEKV7NX5VQDGA',
+                                      :secret_access_key => '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')
+    case type
+    when "low_battery"
+      #low battery
+      ses.send_email(
+        :subject => 'Low Battery',
+        :from => 'hello@apiara.com',
+        :to => 'user@email.com',
+        :body_text => 'Your battery is low.',
+        :body_html => 'Your battery is low.')
+    when "lost_bees"
+      #lost 3 lbs. of bees
+      ses.send_email(
+        :subject => 'Lost Bees',
+        :from => 'hello@apiara.com',
+        :to => 'user@email.com',
+        :body_text => 'Your just lost 3 lbs. of bees.',
+        :body_html => 'Your just lost 3 lbs. of bees.')
+    when "nectar_flow"
+      #nectar is flowing, hive gained 2 lbs.
+      ses.send_email(
+        :subject => 'Nectar Is Flowing!',
+        :from => 'hello@apiara.com',
+        :to => 'user@email.com',
+        :body_text => 'Nectar is flowing! Your hive just gained 2 lbs.',
+        :body_html => 'Nectar is flowing! Your hive just gained 2 lbs.')
+    end
+  end
 end
