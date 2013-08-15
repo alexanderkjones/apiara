4bdd18eabf734fbaecb02616629c8b0d341be225
Anthony Mitchell <awmitchell@gmail.com>
2013-08-15 19:08:20 -0400

    had the "event" value set to "temp"...

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -23,7 +23,7 @@ class DataPoint < AWS::Record::HashModel
     # DataPoint.new(blah)
     DataPoint.create(:id => data["value"]["id"], :date_time => data["value"]["date_time"],
                       :voltage => data["value"]["voltage"], :weight => data["value"]["weight"],
-                      :temp => data["value"]["temp"], :event => data["value"]["temp"])
+                      :temp => data["value"]["temp"], :event => data["value"]["event"])
     
     #time = Time.at(data["date+time"])
     # check if at end of the day. if true, start HiveDays tasks
