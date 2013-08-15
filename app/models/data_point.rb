d3592bff0ee4f33dfcb38813d1a4856359793a06
Anthony Mitchell <awmitchell@gmail.com>
2013-08-15 19:04:05 -0400

    adding raw data to DataPoint table

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -1,6 +1,10 @@
 class DataPoint < AWS::Record::HashModel
-  string_attr :id #HashKey, this will be the hiveid
-  string_attr :raw_data
+  string_attr :id #HashKey
+  string_attr :date_time #RangeKey
+  float_attr :voltage
+  float_attr :weight
+  float_attr :temp
+  string_attr :event
   
   # pick through sent data to get raw data,
   # events, and to kick off HiveDays tasks
@@ -10,13 +14,16 @@ class DataPoint < AWS::Record::HashModel
     puts "---------------"
     puts data["value"]["date_time"]
     puts "---------------"
-    if data["event"]
+    if data["value"]["event"]
       # create new hive_event
       # HiveEvent.new(blah)
     end
     
     # add to raw data store
     # DataPoint.new(blah)
+    DataPoint.create(:id => data["value"]["id"], :date_time => data["value"]["date_time"],
+                      :voltage => data["value"]["voltage"], :weight => data["value"]["weight"],
+                      :temp => data["value"]["temp"], :event => data["value"]["temp"])
     
     #time = Time.at(data["date+time"])
     # check if at end of the day. if true, start HiveDays tasks
