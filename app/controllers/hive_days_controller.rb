3822cccecbb6a5fc77467d6319b327a23e7c3035
Anthony Mitchell <awmitchell@gmail.com>
2013-09-10 20:57:38 -0400

    new dash. sloppy, but you can see where it's heading.

--- app/controllers/hive_days_controller.rb
+++ app/controllers/hive_days_controller.rb
@@ -1,4 +1,6 @@
 class HiveDaysController < ApplicationController
+  layout "dashboard", except: [:index, :hive_days_range]
+  
   def index
     @hive_days = HiveDay.all
 
@@ -10,33 +12,8 @@ class HiveDaysController < ApplicationController
   
   # grabs HiveDay from yesterday so it can be used by the view
   def dash
-    # #table connection stuff
-    # db = AWS::DynamoDB.new
-    # table = db.tables['apiara_development_HiveDay']
-#     
-    # table.hash_key = [:id, :string]
-    # table.range_key = [:date_time, :string]
-#     
-    # @the_hash_key = "sample" # make this dynamic later
-    # @range_low = 1.5.days.ago.to_i.to_s
-    # @range_high = Time.now.to_i.to_s
-#     
-    # #get the latest hive_day
-    # @hive_day = []
-    # collection = table.items.query(:hash_value => @the_hash_key, :range_value => @range_low..@range_high, 
-                                    # :select => [:id, :date_time, :population, :population_cumulative,
-                                                # :production, :production_cumulative, :data_snapshots])
-#                                                 
-    # collection.each do |item_data|
-      # @hive_day.push(item_data.attributes)
-    # end
-#     
-    # # annoying quirk with how items are queried makes this necessary
-    # @hive_day = @hive_day.pop
-
     respond_to do |format|
       format.html # dash.html.erb
-#      format.json { render json: @hive_day }
     end
   end
   
